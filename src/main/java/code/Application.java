e3e991b2fd9bc9d0a70a87d5d69831e8d0b90714
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-16 22:02:48 +0700

    add 'try-with-resources'

--- src/main/java/code/Application.java
+++ src/main/java/code/Application.java
@@ -6,33 +6,32 @@ import java.sql.SQLException;
 public class Application {
     public static void main(String[] args) throws SQLException {
 
-        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+        try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
 
-        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+            try (var statement = conn.createStatement()) {
+                statement.execute(sql);
+            }
 
-        var statement = conn.createStatement();
-        statement.execute(sql);
-        statement.close();
+            var sql2 = "INSERT INTO users (username, phone) VALUES ('Tom', '123456789')";
+            try (var statement2 = conn.createStatement()) {
+                statement2.executeUpdate(sql2);
+            }
 
-        var sql2 = "INSERT INTO users (username, phone) VALUES ('Tom', '123456789')";
-        var statement2 = conn.createStatement();
-        statement2.executeUpdate(sql2);
-        statement2.close();
+            var sql3 = "INSERT INTO users (username, phone) VALUES ('Rob', '987654321')";
+            try (var statement3 = conn.createStatement()) {
+                statement3.executeUpdate(sql3);
+            }
 
-        var sql3 = "INSERT INTO users (username, phone) VALUES ('Rob', '987654321')";
-        var statement3 = conn.createStatement();
-        statement3.executeUpdate(sql3);
-        statement3.close();
-
-        var sql4 = "SELECT * FROM users";
-        var statement4 = conn.createStatement();
-        var resultSet = statement4.executeQuery(sql4);
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString("username"));
-            System.out.println(resultSet.getString("phone"));
+            var sql4 = "SELECT * FROM users";
+            try (var statement4 = conn.createStatement()) {
+                var resultSet = statement4.executeQuery(sql4);
+                while (resultSet.next()) {
+                    System.out.printf("%s %s\n",
+                            resultSet.getString("username"),
+                            resultSet.getString("phone"));
+                }
+            }
         }
-        statement4.close();
-
-        conn.close();
     }
 }
