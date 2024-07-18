eb6aaa4d3d56fbb0140184d601c2dfddafa2ecbf
Daria Polubenko <daria.polubenko@gmail.com>
2024-07-18 19:32:05 +0700

    add class 'User' for create DAO

---
+++ src/main/java/code/User.java
@@ -0,0 +1,42 @@
+package code;
+
+public class User {
+    private int id;
+    private String username;
+    private String phone;
+
+
+    User() {}
+
+    User (String username, String phone) {
+        this.username = username;
+        this.phone = phone;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
